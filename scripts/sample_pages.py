b4d8e84e7a59274de2eb84e01e516cc85dd84e44
Väinö Yrjänäinen <vainoyrjanainen@icloud.com>
2021-06-03 14:42:37 +0200

    Write a description of sampling script

--- scripts/sample_pages.py
+++ scripts/sample_pages.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import os
 import argparse
-def main(args, decades=[1920,1930]):
+def generate_sample(args):
     train = args.train
     head_len = args.head
+    decades = range(args.start, args.end, 10)
     pages_path = "input/protocols/pages.csv"
 
     pages = pd.read_csv(pages_path)
@@ -16,8 +17,9 @@ def main(args, decades=[1920,1930]):
 
         decade_pages = pages[(pages["year"] >= decade) & (pages["year"] < decade_upper)]
         decade_pages = decade_pages.sort_values("ordinal")
-
-        head = decade_pages.head(head_len * 2)
+        
+        tail = decade_pages.tail(args.skip * 2)
+        head = tail.head(head_len * 2)
         if train:
             head = head.iloc[::2, :]
         else:
@@ -30,15 +32,16 @@ def main(args, decades=[1920,1930]):
     return df
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Generate a random sample of pages in the parliamentary corpus.')
     parser.add_argument('--out', type=str, default="sample.csv")
-    parser.add_argument('--train', type=bool, default=True)
-    parser.add_argument('--head', type=int, default=25)
+    parser.add_argument('--train', type=bool, default=True, help="Whether to generate a train or a test set. Boolean.")
+    parser.add_argument('--head', type=int, default=25, help="How many pages are sampled by decade")
+    parser.add_argument('--skip', type=int, default=0, help="How many pages are skipped in the beginning")
+    parser.add_argument('--start', type=int, default=1920, help="Start decade")
+    parser.add_argument('--end', type=int, default=1930, help="End decade")
     args = parser.parse_args()
 
-    decades = range(1920, 1990, 10)
-    df = main(args, decades=decades)
-
+    df = generate_sample(args)
     print(df)
 
     df.to_csv(args.out, index=False)
