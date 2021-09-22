0f12f99cd43cb2b4940980bbbaf1fff1fe095b0b
Robin Saberi <robin.saberinasseri@gmail.com>
2021-09-22 12:33:26 +0200

    takes chambers and manual curation into account

--- scripts/talman_dates.py
+++ scripts/talman_dates.py
@@ -6,78 +6,103 @@ from datetime import datetime, timedelta
 Used for algorithmically providing dates for the (vice) talmän dataset.
 """
 
-# Load and format data
+### Load and format talman data
 talman = pd.read_csv('corpus/talman.csv')
 talman["start"] = talman["start"].fillna('9999')
 talman["end"] = talman["end"].fillna('9999')
-talman["start"] = talman["start"].astype(str)
-talman["end"] = talman["end"].astype(str)
-talman["end"] = list(map(lambda x: x.split('.')[0], talman["end"]))
+talman["start_manual"] = np.ones(len(talman), dtype=int)
+talman["end_manual"] = np.ones(len(talman), dtype=int)
 
+### Load and format riksdagen_dates data
 rd_dates = pd.read_csv('corpus/riksdagen_dates.csv')
-rd_dates = rd_dates.dropna(subset=['start'])
-rd_dates = rd_dates.drop_duplicates(subset='start')
 
+# Remove missing starting dates 
+rd_dates = rd_dates[rd_dates["start"].apply(lambda x: len(x) > 4)]
+
+# Remove urtima files to avoid duplicate year-chamber pairs
+rd_dates = rd_dates[~rd_dates["file"].str.contains("urtima")]
+
+# Create year and chamber columns for matching
 years = rd_dates["start"].str.split('-')
 rd_dates["year"] = [year[0] for year in years]
-rd_dates = rd_dates.groupby('year').filter(lambda x : len(x)==1)
-rd_dates = rd_dates.reset_index()
 
+rd_dates["chamber"] = pd.Series(dtype=str)
+
+# Have duplicates but should be fine when taking chambers into account
+#rd_dates = rd_dates.drop_duplicates(subset='start')
+
+# Create new vars
+rd_dates = rd_dates.reset_index()
 rd_dates["end"] = pd.Series(dtype=str)
 
-# Reformat days to YYYY-MM-DD
 for i in range(len(rd_dates)):
+	if 'fk' in rd_dates.loc[i,"file"]:
+		rd_dates.loc[i,"chamber"] = 'Första kammaren'
 
-	# Format and store starting date
+	elif 'ak' in rd_dates.loc[i,"file"]:
+		rd_dates.loc[i,"chamber"] = 'Andra kammaren'
+
+	else:
+		rd_dates.loc[i,"chamber"] = 'Enkammarriksdagen'
+
+# Make periods end 1 day before another begins
+for i in range(len(rd_dates)):
 	date = rd_dates.loc[i,"start"]
-	year,month,day = date.split('-')
-	date = '-'.join([year,day,month])
-	rd_dates.loc[i,"start"] = date
-
+	
 	# Convert datetime in order to back a day
-	date = datetime.strptime(date, '%Y-%d-%m')
+	date = datetime.strptime(date, '%Y-%m-%d')
 	date = str(date - timedelta(days = 1)).split(' ')[0]
-
-	year,month,day = date.split('-')
-	date = '-'.join([year,day,month])
 
 	rd_dates.loc[i,"end"] = date
 
-# Find non-overlapping stard-end years
+## Find non-overlapping stard-end years
 start_no_overlap = list(set(talman["start"]) - set(talman["end"]))
 end_no_overlap = list(set(talman["end"]) - set(talman["start"]))
 
+# Filter away manually added dates
+start_no_overlap = [date for date in start_no_overlap if len(date) <= 4]
+end_no_overlap = [date for date in end_no_overlap if len(date) <= 4]
+
+# Add dates algorithmically
 for i in range(len(talman)):
-	talman_start, talman_end = talman.loc[i, ["start", "end"]]
-	
-	# If no one ends when someone starts, make starting date 'YYYY-01-01'
-	if talman_start in start_no_overlap:
-		talman.loc[i, "start"] = '-'.join([talman_start, '01', '01'])
+	talman_start, talman_end, chamber = talman.loc[i, ["start", "end", "chamber"]]
 
-	# Else make date day of starting the riksdagen year
-	else:
-		id_start = np.where(rd_dates["year"] == talman_start)[0]
-		# In few cases we have multiple 1st protocols, skip these
-		if len(id_start) == 1:
-			talman.loc[i,"start"] = rd_dates.loc[id_start[0],"start"]
+	# Ignore manually added dates
+	if len(talman_start) <= 4:
 
-	# If no one starts when someone ends, make starting date 'YYYY-31-12'
-	if talman_end in end_no_overlap:
-		talman.loc[i, "end"] = '-'.join([talman_end, '31', '12'])
+		# If no one ends when someone starts, make starting date 'YYYY-01-01'
+		if talman_start in start_no_overlap:
+			talman.loc[i, "start"] = '-'.join([talman_start, '01', '01'])
+			talman.loc[i, "start_manual"] = 0
 
-	# Else make date day BEFORE starting of the NEXT riksdagen year
-	else:
-		id_end = np.where(rd_dates["year"] == talman_end)[0]
-		# In few cases we have multiple 1st protocols, skip these
-		if len(id_end) == 1:
-			talman.loc[i,"end"] = rd_dates.loc[id_end[0],"end"]
+		# Else make date day of starting the riksdagen year
+		else:
+			id_start = np.where((rd_dates["year"] == talman_start) & (rd_dates["chamber"] == chamber))[0]
 
-talman.loc[talman["start"] == '9999-01-01', "start"] = np.nan
-talman.loc[talman["end"] == '9999-31-12', "end"] = np.nan
+			# Skip non-matches
+			if len(id_start) != 0:
+				talman.loc[i,"start"] = rd_dates.loc[id_start[0],"start"]
+				talman.loc[i, "start_manual"] = 0
 
+	if len(talman_end) <= 4:
+		# If no one ends when someone starts, make starting date 'YYYY-01-01'
+		if talman_end in end_no_overlap:
+			talman.loc[i, "end"] = '-'.join([talman_end, '12', '31'])
+			talman.loc[i, "end_manual"] = 0
+
+		# Else make date day of starting the riksdagen year
+		else:
+			id_end = np.where((rd_dates["year"] == talman_end) & (rd_dates["chamber"] == chamber))[0]
+
+			# Skip non-matches
+			if len(id_end) != 0:
+				talman.loc[i,"end"] = rd_dates.loc[id_end[0],"end"]
+				talman.loc[i, "end_manual"] = 0
+
+# Cleanup and save
+talman.loc[talman["start"] == '9999', "start"] = np.nan
+talman.loc[talman["end"] == '9999', "end"] = np.nan
 talman.to_csv('corpus/talman_new.csv', index=False)
-
-		
 
 
 
