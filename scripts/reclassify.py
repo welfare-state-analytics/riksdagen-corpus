43db6398c1fee4658c4b6bf106173dac3f8e4932
ninpnin <vainoyrjanainen@icloud.com>
2021-12-07 18:18:38 +0100

    Use proper classifier in reclassifying script

--- scripts/reclassify.py
+++ scripts/reclassify.py
@@ -9,15 +9,54 @@ from lxml import etree
 import pandas as pd
 import os, progressbar, sys
 import argparse
+import numpy as np
+
+def classify_paragraph(s, model, ft, dim, prior=np.log([0.8, 0.2])):
+    if s is None:
+        return "note"
+    words = s.split()
+    V = len(words)
+    x = np.zeros((V, dim))
+
+    for ix, word in enumerate(words):
+        vec = ft.get_word_vector(word)
+        x[ix] = vec
+
+    pred = model.predict(x, batch_size=V)
+    biases = model.predict(np.zeros(x.shape), batch_size=V)
+    # print(pred)
+    prediction = np.sum(pred, axis=0) + prior
+
+    if prediction[0] < prediction[1]:
+        #print("note", s.strip()[:100])
+        return "note"
+    else:
+        #print("u", s.strip()[:100])
+        return "u"
+
+def get_neural_classifier(model, ft, dim):
+    return (lambda paragraph: classify_paragraph(paragraph, model, ft, dim))
 
 def main(args):
     parser = etree.XMLParser(remove_blank_text=True)
+
+    if False:
+        classifier = random_classifier
+    else:
+        # Do imports here because they take a loong time
+        from tensorflow import keras
+        import fasttext, fasttext.util
+        dim = 300
+        fasttext.util.download_model('sv', if_exists='ignore')
+        ft = fasttext.load_model("cc.sv." + str(dim) + ".bin")
+        model = keras.models.load_model('input/segment-classifier/')
+        classifier = get_neural_classifier(model, ft, dim)
 
     for protocol_path in progressbar.progressbar(list(protocol_iterators("corpus/", start=args.start, end=args.end))):
         metadata = infer_metadata(protocol_path)
         root = etree.parse(protocol_path, parser).getroot()
 
-        root = reclassify(root, random_classifier, exclude=["date", "speaker"])
+        root = reclassify(root, classifier, exclude=["date", "speaker"])
         root = format_texts(root)
         b = etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
 
