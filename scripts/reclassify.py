0e5540688c822c11c8d9d894fc57ab21cc50bb5f
ninpnin <vainoyrjanainen@icloud.com>
2021-12-07 12:43:47 +0100

    No longer reclassify speaker or date elements

--- scripts/reclassify.py
+++ scripts/reclassify.py
@@ -4,55 +4,30 @@ Run the classification into utterances and notes.
 from pyparlaclarin.refine import reclassify, format_texts, random_classifier
 
 from pyriksdagen.db import filter_db, load_patterns
-from pyriksdagen.utils import infer_metadata
+from pyriksdagen.utils import infer_metadata, protocol_iterators
 from lxml import etree
 import pandas as pd
 import os, progressbar, sys
-
-parser = etree.XMLParser(remove_blank_text=True)
-
-
-def reclassify_protocol(folder, protocol_id):
-    metadata = infer_metadata(protocol_id)
-    filename = folder + protocol_id + ".xml"
-    root = etree.parse(filename, parser).getroot()
-
-    root = reclassify(root, random_classifier)
-    root = format_texts(root)
-    b = etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
-
-    f = open(filename, "wb")
-    f.write(b)
-    f.close()
-
+import argparse
 
 def main(args):
-    pc_folder = "corpus/"
-    if len(args) >= 2:
-        for filename in args[1:]:
-            folder = "/".join(filename.split("/")[:-1]) + "/"
-            protocol_id = filename.split("/")[-1].replace(".xml", "")
-            assert filename == folder + protocol_id + ".xml", (
-                folder + protocol_id + ".xml"
-            )
-            reclassify_protocol(folder, protocol_id)
+    parser = etree.XMLParser(remove_blank_text=True)
 
-    else:
-        folders = os.listdir(pc_folder)
-        for outfolder in progressbar.progressbar(folders):
-            if os.path.isdir(pc_folder + outfolder):
-                outfolder = outfolder + "/"
-                protocol_ids = os.listdir(pc_folder + outfolder)
-                protocol_ids = [
-                    protocol_id.replace(".xml", "")
-                    for protocol_id in protocol_ids
-                    if protocol_id.split(".")[-1] == "xml"
-                ]
+    for protocol_path in progressbar.progressbar(list(protocol_iterators("corpus/", start=args.start, end=args.end))):
+        metadata = infer_metadata(protocol_path)
+        root = etree.parse(protocol_path, parser).getroot()
 
-                for protocol_id in protocol_ids:
-                    reclassify_protocol(pc_folder + outfolder, protocol_id)
+        root = reclassify(root, random_classifier, exclude=["date", "speaker"])
+        root = format_texts(root)
+        b = etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
 
+        f = open(protocol_path, "wb")
+        f.write(b)
+        f.close()
 
 if __name__ == "__main__":
-    args = sys.argv
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--start", type=int, default=1920)
+    parser.add_argument("--end", type=int, default=2021)
+    args = parser.parse_args()
     main(args)
