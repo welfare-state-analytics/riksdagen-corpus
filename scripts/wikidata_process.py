7a86986ac2ad0fade8f8be944e699afd30455c62
Robin Saberi <robin.saberinasseri@gmail.com>
2022-02-16 14:05:26 +0100

    queries properly adding (prime-)ministers

--- scripts/wikidata_process.py
+++ scripts/wikidata_process.py
@@ -5,47 +5,59 @@ import numpy as np
 import pandas as pd
 import os, json
 from pyriksdagen.mp import add_id
+from pyriksdagen.match_mp import clean_names
 import unicodedata
 from datetime import datetime
 
-#government = pd.read_csv(os.path.join('corpus', 'government.csv'))
-individual = pd.read_csv(os.path.join('corpus', 'individual.csv'))
-location = pd.read_csv(os.path.join('corpus', 'location.csv'))
-member = pd.read_csv(os.path.join('corpus', 'member.csv'))
-minister = pd.read_csv(os.path.join('corpus', 'minister.csv'))
-name = pd.read_csv(os.path.join('corpus', 'name.csv'))
-party = pd.read_csv(os.path.join('corpus', 'party_affiliation.csv'))
-prime_minister = pd.read_csv(os.path.join('corpus', 'prime_minister.csv'))
-speaker = pd.read_csv(os.path.join('corpus', 'speaker.csv'))
+government = pd.read_csv('corpus/metadata/government.csv')
+person = pd.read_csv('corpus/metadata/person.csv')
+location_specifier = pd.read_csv('corpus/metadata/location_specifier.csv')
+member = pd.read_csv('corpus/metadata/member_of_parliament.csv')
+minister = pd.read_csv('corpus/metadata/minister.csv')
+name = pd.read_csv('corpus/metadata/name.csv')
+party = pd.read_csv('corpus/metadata/party_affiliation.csv')
+speaker = pd.read_csv('corpus/metadata/speaker.csv')
+party_map = pd.read_csv('corpus/metadata/party_abbreviation.csv')
 
-# Drop 1 character names
-name["name"] = name["name"].apply(lambda x: ' '.join([i for i in x.split() if len(i) > 1]))
-name = name.loc[name["name"] != ''].reset_index(drop=True)
-
-# Map gender to english
-individual["gender"] = individual["gender"].map({'kvinna':'woman', 'man':'man'})
-
+### Process corpus data
 # Drop parties never present in riksdagen
-with open('corpus/party_mapping.json', 'r') as f:
-	party_map = json.load(f)
+party_map = {row["party"]:row["abbreviation"] for _, row in party_map.iterrows()}
 party["party_abbrev"] = party["party"].map(party_map)
 party = party.dropna().reset_index(drop=True)
 
-# Prime ministers
-prime_minister = prime_minister.merge(individual, on='wiki_id', how='left')
-prime_minister = prime_minister.merge(location, on='wiki_id', how='left')
-prime_minister = prime_minister.merge(name, on='wiki_id', how='left')
+# Name
+name["name"] = name["name"].str.replace('"','')
+name["name"] = name["name"].apply(clean_names)
+name = name.loc[name["name"] != ''].reset_index(drop=True)
+
+# Map gender to english
+person["gender"] = person["gender"].map({'kvinna':'woman', 'man':'man'})
+
+### Create dbs for matching
+# Minister
+government[["start", "end"]] = government[["start", "end"]].apply(pd.to_datetime, errors="coerce")
+government.loc[government["start"] == max(government["start"]), "end"] = datetime.strptime('2022-12-31', '%Y-%m-%d')
+government = government.rename(columns={'start':'gov_start', 'end':'gov_end'})
 
 # Ministers
-minister = minister.merge(individual, on='wiki_id', how='left')
-minister = minister.merge(location, on='wiki_id', how='left')
+minister = minister.merge(person, on='wiki_id', how='left')
+minister = minister.merge(location_specifier, on='wiki_id', how='left')
 minister = minister.merge(name, on='wiki_id', how='left')
-minister = minister.append(prime_minister) # add prime ministers
+minister = minister.merge(government, on='government', how='left')
 minister["role"] = minister["role"].str.replace('Sveriges ', '')
 
+# Impute end dates for ministers currently in office
+idx = np.where(minister["gov_start"] == max(minister["gov_start"]))[0]
+idy = minister["end"].isna()
+idx = [i for i in idx if i in idy]
+minister.loc[idx, "end"] = '2022-12-31'
+
+# No idea why but name is missing here, debug later
+minister.loc[minister["wiki_id"] == 'Q60971016'] = 'Gerhard Larsson'
+
 # Speakers
-speaker = speaker.merge(individual, on='wiki_id', how='left')
-speaker = speaker.merge(location, on='wiki_id', how='left')
+speaker = speaker.merge(person, on='wiki_id', how='left')
+speaker = speaker.merge(location_specifier, on='wiki_id', how='left')
 speaker = speaker.merge(name, on='wiki_id', how='left')
 speaker["role"] = speaker["role"].map({
 	'Sveriges riksdags talman':'speaker',
@@ -60,14 +72,14 @@ speaker["role"] = speaker["role"].map({
 	})
 
 # Members
-member = member.merge(individual, on='wiki_id', how='left')
-member = member.merge(location, on='wiki_id', how='left')
+member = member.merge(person, on='wiki_id', how='left')
+member = member.merge(location_specifier, on='wiki_id', how='left')
 member = member.merge(name, on='wiki_id', how='left')
 
 # Clean values
 member["role"] = member["role"].str.extract(r'([A-Za-zÀ-ÿ]*ledamot)')
 
-# Impute missing party values for members
+# Impute missing party values for members (not used for others atm)
 idx = member["party"].isnull()
 missing = member.loc[idx]
 missing = missing.merge(party, on='wiki_id', how='left')
@@ -85,7 +97,36 @@ member = member.loc[~member["end"].isna()].reset_index(drop=True)
 member.loc[~member["start"].str.contains('http')].reset_index(drop=True)
 member.loc[~member["end"].str.contains('http')].reset_index(drop=True)
 
+# Map party_abbrev and chamber
+member["party_abbrev"] = member["party"].map(party_map)
+member["chamber"] = member["role"].map({'ledamot':'Enkammarriksdagen',
+                                         'förstakammarledamot':'Första kammaren',
+                                         'andrakammarledamot':'Andra kammaren'})
+
+# Switch from wiki_id to person_id
+person["person_id"] = person["wiki_id"] # Temporary id
+d = {row["wiki_id"]:row["person_id"] for _, row in person.iterrows()}
+files_to_modify = ['person', 'location_specifier', 'member_of_parliament', 'minister', 'name', 'party_affiliation', 'speaker']
+
+for file in files_to_modify:
+	df = pd.read_csv(f'corpus/metadata/{file}.csv')
+	df['person_id'] = df['wiki_id'].map(d)
+#	if file != 'person':
+#		df.drop('wiki_id', axis=1)
+	df.to_csv(f'corpus/metadata/{file}.csv', index=False)
+
+# Also on aggregated files
+member['person_id'] = member['wiki_id']
+minister['person_id'] = minister['wiki_id']
+speaker['person_id'] = speaker['wiki_id']
+member.drop('wiki_id', axis=1)
+minister.drop('wiki_id', axis=1)
+speaker.drop('wiki_id', axis=1)
+
 # Save files used for matching
-member.to_csv('input/matching/member.csv', index=False)
+member = member.drop_duplicates()
+minister = minister.drop_duplicates()
+speaker = speaker.drop_duplicates()
+member.to_csv('input/matching/member_of_parliament.csv', index=False)
 minister.to_csv('input/matching/minister.csv', index=False)
 speaker.to_csv('input/matching/speaker.csv', index=False)
--- scripts/wikidata_query.py
+++ scripts/wikidata_query.py
@@ -3,11 +3,13 @@ Creates csv files from wikidata queries, with cleaning limited to simple reforma
 '''
 from SPARQLWrapper import SPARQLWrapper, JSON
 import pandas as pd
-import os
+import os, argparse
 import time
 import re
 
 def query2df(query: str):
+	'''TODO: Change delimiter=";" to avoid splitting names incorrectly'''
+	sparql = SPARQLWrapper("https://query.wikidata.org/sparql", agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11")
 	sparql.setQuery(query)	
 	sparql.setReturnFormat(JSON)
 	results = sparql.query().convert()
@@ -21,76 +23,85 @@ def try_query(query: str, max_tries=3):
 			return df
 		except:
 			time.sleep(3)
-	raise
+	raise ValueError('Query timeout/forbidden.')
 
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-queries = sorted([q for q in os.listdir(os.path.join('input', 'queries')) if q.endswith('.rq')])
+def main(args):
+	for query in args.queries:
+		with open(os.path.join('input', 'queries', query), 'r') as f:
+			df = query2df(f.read())
+		
+		# Drop columns
+		df = df[[c for c in df.columns if c.endswith('.value')]]
+		df.columns = df.columns.str.replace('.value', '', regex=False)
 
-for query in queries:
-	with open(os.path.join('input', 'queries', query), 'r') as f:
-		df = try_query(f.read())
+		# Rename columns
+		try:
+			df["wiki_id"] = df["wiki_id"].str.replace(r'(.*?\/)', '',  regex=True)
+		except:
+			pass
+		df = df.rename(columns={'wiki_idLabel':'name'}) # name.rq
+		df.columns = df.columns.str.replace('Label', '', regex=False)
 
-	# Drop columns
-	df = df[[c for c in df.columns if c.endswith('.value')]]
-	df.columns = df.columns.str.replace('.value', '', regex=False)
+		if query == 'minister.rq':
+			df["role"] = df["role"].str.replace('Sveriges', '').str.strip()
 
-	# Rename columns
-	try:
-		df["wiki_id"] = df["wiki_id"].str.replace(r'(.*?\/)', '',  regex=True)
-	except:
-		pass
-	df = df.rename(columns={'wiki_idLabel':'name'}) # name.rq
-	df.columns = df.columns.str.replace('Label', '', regex=False)
+		# Format dates
+		date_cols = [c for c in df.columns if c in ['start', 'end', 'born', 'dead', 'date']]
+		for col in date_cols:
+			df[col] = df[col].str.replace(r'T.+', '',  regex=True)
 
-	if query == 'minister.rq':
-		df["role"] = df["role"].str.replace('Sveriges', '').str.strip()
+		if 'riksdagen_id' in df.columns:
+			df['riksdagen_id'] = df['riksdagen_id'].astype(str)
 
-	# Format dates
-	date_cols = [c for c in df.columns if c in ['start', 'end', 'born', 'dead']]
-	for col in date_cols:
-		df[col] = df[col].str.replace(r'T.+', '',  regex=True)
+		if query in ['motion.rq', 'interpellation.rq']:
+			df.to_csv(f"input/metadata{query.replace('.rq', '.csv')}", index=False)
 
-	if query != 'name_location.rq':
-		df.to_csv(os.path.join('corpus', query.replace('.rq', '.csv')), index=False)
-	
-	else:
-		# Make all names into rows
-		name = df
-		name["wiki_idAlt"] = name["wiki_idAlt"].apply(lambda x: x.split(',') if isinstance(x, str) else [])
-		name["name"] = name.apply(lambda x: [x["name"]] + x["wiki_idAlt"], axis=1)
-		name = name.drop("wiki_idAlt", axis=1)
-		name = name.set_index('wiki_id')["name"].apply(pd.Series).stack().reset_index(level=-1, drop=True).astype(str).reset_index()
-		wiki_id = name["wiki_id"]
+		elif query != 'name_location_specifier.rq':
+			df.to_csv(f"corpus/metadata/{query.replace('.rq', '.csv')}", index=False)	
 
-		# Separate locations from names
-		name = name[0].str.split(' [io] ', expand=True)
-		name = name.apply(lambda x: x.str.replace('och', '').str.strip())
-		name = name.rename(columns={0:'name'})
-		name["wiki_id"] = wiki_id
-		name["name"] = name["name"].str.replace(r'\(([^\)]+)\)', '', regex=True).str.strip()
+		else:
+			# Make all names into rows
+			name = df
+			name["wiki_idAlt"] = name["wiki_idAlt"].apply(lambda x: x.split(',') if isinstance(x, str) else [])
+			name["name"] = name.apply(lambda x: [x["name"]] + x["wiki_idAlt"], axis=1)
+			name = name.drop("wiki_idAlt", axis=1)
+			name = name.set_index('wiki_id')["name"].apply(pd.Series).stack().reset_index(level=-1, drop=True).astype(str).reset_index()
+			wiki_id = name["wiki_id"]
 
-		location = []
-		for i, row in name.iterrows():
-			for j in range(len(name.columns)-2):
-				if row[j+1] != None:
-					location.append([row["wiki_id"], row[j+1]])
-		location = pd.DataFrame(location, columns=['wiki_id', 'location'])
+			# Separate locations from names
+			name = name[0].str.split(' [io] ', expand=True)
+			name = name.apply(lambda x: x.str.replace('och', '').str.strip())
+			name = name.rename(columns={0:'name'})
+			name["wiki_id"] = wiki_id
+			name["name"] = name["name"].str.replace(r'\(([^\)]+)\)', '', regex=True).str.strip()
 
-		# Extend both files with name_in_riksdagen list
-		alias = pd.read_csv(os.path.join('corpus', 'alias.csv'))
-		idx = alias["alias"].str.split(' i ').apply(lambda x: len(x) == 2)
-		alias = alias.loc[idx].reset_index(drop=True)
-		wiki_id = alias["wiki_id"]
-		alias = alias["alias"].str.split(' i ', expand=True).rename(columns={0:'name', 1:'location'})
-		alias["wiki_id"] = wiki_id
+			location = []
+			for i, row in name.iterrows():
+				for j in range(len(name.columns)-2):
+					if row[j+1] != None:
+						location.append([row["wiki_id"], row[j+1]])
+			location = pd.DataFrame(location, columns=['wiki_id', 'location'])
 
-		name = name.loc[~name["name"].isna()]
-		name = name[['wiki_id', 'name']].append(alias[["wiki_id", "name"]]).drop_duplicates()
-		location = location.append(location[["wiki_id", "location"]]).drop_duplicates()
-		name.to_csv(os.path.join('corpus', 'name.csv'), index=False)
-		location.to_csv(os.path.join('corpus', 'location.csv'), index=False)
-		os.remove(os.path.join('corpus', 'alias.csv'))
+			# Extend both files with name_in_riksdagen list
+			alias = pd.read_csv('corpus/metadata/alias.csv')
+			idx = alias["alias"].str.split(' i ').apply(lambda x: len(x) == 2)
+			alias = alias.loc[idx].reset_index(drop=True)
+			wiki_id = alias["wiki_id"]
+			alias = alias["alias"].str.split(' i ', expand=True).rename(columns={0:'name', 1:'location'})
+			alias["wiki_id"] = wiki_id
 
-	print(f'Query {query} finished.')
+			name = name[['wiki_id', 'name']].append(alias[["wiki_id", "name"]])
+			name = name.loc[~name["name"].isna()].drop_duplicates()
+			location = location.append(location[["wiki_id", "location"]]).drop_duplicates()
+			name.to_csv('corpus/metadata/name.csv', index=False)
+			location.to_csv('corpus/metadata/location_specifier.csv', index=False)
+			os.remove('corpus/metadata/alias.csv')
 
-# change Sverige for ministers
+		print(f'Query {query} finished.')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    default = queries = sorted([q for q in os.listdir(os.path.join('input', 'queries')) if q.endswith('.rq')])
+    parser.add_argument('-q', '--queries', default=default, nargs='+', help='One or more sparql query files (separated by space)')
+    args = parser.parse_args()
+    main(args)
