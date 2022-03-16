660226ff6248fa2ad2c5bd390778c7a51b7fe8ea
Robin Saberi Nasseri <robsa662@uuc-02g208qq6lw.lan>
2022-03-16 11:37:38 +0100

    computer is hotter, runtime is quicker

--- scripts/redetect.py
+++ scripts/redetect.py
@@ -20,70 +20,80 @@ from pyriksdagen.refine import (
 from pyriksdagen.utils import infer_metadata, parse_date
 from pyriksdagen.utils import protocol_iterators
 from pyriksdagen.match_mp import clean_names
+from tqdm import tqdm
+from multiprocessing import Pool
 
-def main(args):
-    tei_ns = ".//{http://www.tei-c.org/ns/1.0}"
+def redetect_protocol(protocol):
     party_mapping, mp_db, minister_db, speaker_db = load_metadata()
 
-    unknown_variables = ["gender", "party", "other"]
+    protocol_id = protocol.split("/")[-1]
+    metadata = infer_metadata(protocol)
+    root = etree.parse(protocol, parser).getroot()
+    
+    # Year from the folder name
+    year = metadata["year"]
+    # Take into account folders such as 198889
+    secondary_year = metadata.get("secondary_year", year)
+
+    dates = [
+        parse_date(elem.attrib.get("when"))
+        for elem in root.findall(tei_ns + "docDate")
+        if parse_date(elem.attrib.get("when")).year in [year, secondary_year]
+    ]
+    
+    # Dates from xml is wrong for digitized era        
+    if len(dates) > 0:
+        start_date, end_date = min(dates), max(dates)          
+
+    else:
+        start_date = datetime(year,1,1)
+        end_date = datetime(secondary_year,12,31)
+    
+    year_mp_db = filter_db(mp_db, start_date=start_date, end_date=end_date)
+    year_minister_db = filter_db(minister_db, start_date=start_date, end_date=end_date)
+    year_speaker_db = filter_db(speaker_db, start_date=start_date, end_date=end_date)
+    
+    # Introduction patterns
+    pattern_db = load_patterns()
+    pattern_db = pattern_db[
+        (pattern_db["start"] <= year) & (pattern_db["end"] >= year)
+    ]
+    
+    root, unk = detect_mps(
+        root,
+        None,
+        pattern_db,
+        mp_db=year_mp_db,
+        minister_db=year_minister_db,
+        speaker_db=year_speaker_db,
+        metadata=metadata,
+        party_map=party_mapping,
+        protocol_id=protocol_id,
+        unknown_variables=unknown_variables,
+    )
+
+    b = etree.tostring(
+        root, pretty_print=True, encoding="utf-8", xml_declaration=True
+    )
+
+    f = open(protocol, "wb")
+    f.write(b)
+    f.close()
+    return unk
+
+
+# Global variables
+tei_ns = ".//{http://www.tei-c.org/ns/1.0}"
+unknown_variables = ["gender", "party", "other"]
+parser = etree.XMLParser(remove_blank_text=True)
+
+
+def main(args):
+    protocols = list(protocol_iterators("corpus/protocols/", start=args.start, end=args.end))
     unknowns = []
-    parser = etree.XMLParser(remove_blank_text=True)
-
-    for protocol in progressbar.progressbar(list(protocol_iterators("corpus/protocols/", start=args.start, end=args.end))):
-        protocol_id = protocol.split("/")[-1]
-        metadata = infer_metadata(protocol)
-        root = etree.parse(protocol, parser).getroot()
-        
-        # Year from the folder name
-        year = metadata["year"]
-        # Take into account folders such as 198889
-        secondary_year = metadata.get("secondary_year", year)
-
-        dates = [
-            parse_date(elem.attrib.get("when"))
-            for elem in root.findall(tei_ns + "docDate")
-            if parse_date(elem.attrib.get("when")).year in [year, secondary_year]
-        ]
-        
-        # Dates from xml is wrong for digitized era
-        if len(dates) > 0:
-            start_date, end_date = min(dates), max(dates)           
-        else:
-            start_date = datetime(year,1,1)
-            end_date = datetime(secondary_year,12,31)
-        
-        year_mp_db = filter_db(mp_db, start_date=start_date, end_date=end_date)
-        year_minister_db = filter_db(minister_db, start_date=start_date, end_date=end_date)
-        year_speaker_db = filter_db(speaker_db, start_date=start_date, end_date=end_date)
-        
-        # Introduction patterns
-        pattern_db = load_patterns()
-        pattern_db = pattern_db[
-            (pattern_db["start"] <= year) & (pattern_db["end"] >= year)
-        ]
-        
-        root, unk = detect_mps(
-            root,
-            None,
-            pattern_db,
-            mp_db=year_mp_db,
-            minister_db=year_minister_db,
-            speaker_db=year_speaker_db,
-            metadata=metadata,
-            party_map=party_mapping,
-            protocol_id=protocol_id,
-            unknown_variables=unknown_variables,
-        )
-
+    pool = Pool()
+    for unk in tqdm(pool.imap(redetect_protocol, protocols), total=len(protocols)):
         unknowns.extend(unk)
-        b = etree.tostring(
-            root, pretty_print=True, encoding="utf-8", xml_declaration=True
-        )
-
-        f = open(protocol, "wb")
-        f.write(b)
-        f.close()
-
     unknowns = pd.DataFrame(unknowns, columns=['protocol_id', 'hash']+unknown_variables)
     print('Proportion of metadata identified for unknowns:')
     print((unknowns[["gender", "party", "other"]] != '').sum() / len(unknowns))
