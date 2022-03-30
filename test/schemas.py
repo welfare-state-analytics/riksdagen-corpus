b3740536763db2a586c8d3c22c4609fcb633f1c0
ninpnin <vainoyrjanainen@icloud.com>
2022-03-30 10:42:14 +0300

    test: validate schema for all three digital original formats

--- test/schemas.py
+++ test/schemas.py
@@ -18,50 +18,29 @@ class Test(unittest.TestCase):
         self.assertEqual(valid, True)
 
     # Parla-clarin generated from example OCR XML
-    def test_generated_example(self):
+    def test_protocols(self):
         schema_path = "schemas/parla-clarin.xsd"
-        protocol_id = "prot-198990--93"
-        fname = "prot_198990__93-031.xml"
-        
-        # Package argument can be None since the file is already saved on disk
-        content_blocks = get_blocks(protocol_id, None)
-        metadata = infer_metadata(fname.split(".")[0])
-        tei = create_tei(protocol, metadata)
-        parla_clarin_str = create_parlaclarin(tei, metadata)
-        print("pc str created..")
-        
-        parlaclarin_path = "input/parla-clarin/generated-example.xml"
-        f = open(parlaclarin_path, "w")
-        f.write(parla_clarin_str)
-        f.close()
-        
-        valid = validate_xml_schema(parlaclarin_path, schema_path)
-        self.assertEqual(valid, True)
-
-    # Parla-clarin generated from example OCR XML
-    def test_generated_example(self):
-        schema_path = "schemas/parla-clarin.xsd"
-        protocol_id1 = "1955/prot-1955--ak--22" # Andra kammaren
-        protocol_id2 = "1933/prot-1933--fk--5" # Första kammaren
-        protocol_id3 = "197879/prot-197879--14" # Enkammarsriksdagen
-        protocol_id4 = "199596/prot-199596--35" # Digital original
+        protocol_id1, msg1 = "1955/prot-1955--ak--22", "Andra kammaren"
+        protocol_id2, msg2 = "1933/prot-1933--fk--5", "Första kammaren"
+        protocol_id3, msg3 = "197879/prot-197879--14", "Enkammarsriksdagen"
+        protocol_id4, msg4 = "199596/prot-199596--35", "Digital original, format 1"
+        protocol_id5, msg5 = "201011/prot-201011--19", "Digital original, format 2"
+        protocol_id6, msg6 = "201819/prot-201819--45", "Digital original, format 3"
 
         folder = "corpus/protocols/"
-        parlaclarin_path1 = folder + protocol_id1 + ".xml"
-        parlaclarin_path2 = folder + protocol_id2 + ".xml"
-        parlaclarin_path3 = folder + protocol_id3 + ".xml"
-        parlaclarin_path4 = folder + protocol_id4 + ".xml"
+        valid1 = validate_xml_schema(f"{folder}{protocol_id1}.xml", schema_path)
+        valid2 = validate_xml_schema(f"{folder}{protocol_id2}.xml", schema_path)
+        valid3 = validate_xml_schema(f"{folder}{protocol_id3}.xml", schema_path)
+        valid4 = validate_xml_schema(f"{folder}{protocol_id4}.xml", schema_path)
+        valid5 = validate_xml_schema(f"{folder}{protocol_id5}.xml", schema_path)
+        valid6 = validate_xml_schema(f"{folder}{protocol_id6}.xml", schema_path)
 
-        valid1 = validate_xml_schema(parlaclarin_path1, schema_path)
-        valid2 = validate_xml_schema(parlaclarin_path2, schema_path)
-        valid3 = validate_xml_schema(parlaclarin_path3, schema_path)
-        valid4 = validate_xml_schema(parlaclarin_path4, schema_path)
-
-        self.assertEqual(valid1, True)
-        self.assertEqual(valid2, True)
-        self.assertEqual(valid3, True)
-        self.assertEqual(valid4, True)
-
+        self.assertTrue(valid1, f"{msg1}: {protocol_id1}")
+        self.assertTrue(valid2, f"{msg2}: {protocol_id2}")
+        self.assertTrue(valid3, f"{msg3}: {protocol_id3}")
+        self.assertTrue(valid4, f"{msg4}: {protocol_id4}")
+        self.assertTrue(valid5, f"{msg5}: {protocol_id5}")
+        self.assertTrue(valid6, f"{msg6}: {protocol_id6}")
 
 if __name__ == '__main__':
     # begin the unittest.main()
