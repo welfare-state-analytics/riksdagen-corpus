e492bff1eca469cc83ea3c9b49da0de6df941ccc
ninpnin <vainoyrjanainen@icloud.com>
2022-05-05 16:38:23 +0200

    fix: merge two scripts

--- scripts/accuracy_version_plot.py
+++ scripts/accuracy_version_plot.py
@@ -2,40 +2,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 from cycler import cycler
+import re
+
+exp = "v([0-9]+)([.])([0-9]+)([.])([0-9]+)(b|rc)?([0-9]+)?"
+exp = re.compile(exp)
 
 
 def update_plot(version):
-	default_cycler = (cycler(color=['r', 'g', 'b', 'y']) +
-	                  cycler(linestyle=['-', '--', ':', '-.']))
-	plt.rc('axes', prop_cycle=default_cycler)
-	f, ax = plt.subplots()
+    default_cycler = (cycler(color=['r', 'g', 'b', 'y']) +
+                      cycler(linestyle=['-', '--', ':', '-.']))
+    plt.rc('axes', prop_cycle=default_cycler)
+    f, ax = plt.subplots()
 
-	df = pd.read_csv('input/accuracy/difference.csv')
+    df = pd.read_csv('input/accuracy/difference.csv')
 
-	# Overwrite current version
-	if len(df[df['version'] == version]) > 1:
-		df = df[df['version'] != version]
+    # Overwrite current version
+    if len(df[df['version'] == version]) > 1:
+        df = df[df['version'] != version]
 
-	# Add current version
-	accuracy = pd.read_csv('input/accuracy/upper_bound.csv')
-	accuracy = accuracy[['year', 'accuracy_upper_bound']].rename(columns={'accuracy_upper_bound':'accuracy'})
-	accuracy['version'] = version
-	df = pd.concat([df, accuracy])
+    # Add current version
+    accuracy = pd.read_csv('input/accuracy/upper_bound.csv')
+    accuracy = accuracy[['year', 'accuracy_upper_bound']].rename(columns={'accuracy_upper_bound':'accuracy'})
+    accuracy['version'] = version
+    df = pd.concat([df, accuracy])
 
-	version = sorted(list(set(df['version'])), reverse=True)
-	for v in version:
-		dfv = df.loc[df['version'] == v]
-		x = dfv['year'].tolist()
-		y = dfv['accuracy'].tolist()
-		x, y = zip(*sorted(zip(x,y),key=lambda x: x[0]))
-		plt.plot(x, y)
+    version = sorted(list(set(df['version'])), reverse=True)
+    for v in version:
+        dfv = df.loc[df['version'] == v]
+        x = dfv['year'].tolist()
+        y = dfv['accuracy'].tolist()
+        x, y = zip(*sorted(zip(x,y),key=lambda x: x[0]))
+        plt.plot(x, y)
 
-	plt.title('Estimated accuracy for identification of speech-maker')
-	plt.legend(version, loc ="upper left")
-	ax.set_xlabel('Year')
-	ax.set_ylabel('Accuracy')
-	return f, ax
-
+    plt.title('Estimated accuracy for identification of speech-maker')
+    plt.legend(version, loc ="upper left")
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Accuracy')
+    return f, ax
 
 def main(args):
 	f, ax = update_plot(args.version)
@@ -51,5 +54,10 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--show", type=str, default="True")
     args = parser.parse_args()
     args.show = False if args.show.lower()[:1] == "f" else True
-    main(args)
+    if exp.search(args.version) is None:
+        print(f"{args.version} is not a valid version number. Exiting")
+        exit()
+    else:
+        args.version = exp.search(args.version).group(0)
+        main(args)
 
