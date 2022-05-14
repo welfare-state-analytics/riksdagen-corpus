40fdc9d90972a6091e2d9e919392fd4ab13b0b95
Robin Saberi Nasseri <robsa662@uuc-02g208qq6lw.lan>
2022-05-14 15:53:05 +0200

    fix: bug where secondary mp_db always was empty

--- pyriksdagen/refine.py
+++ pyriksdagen/refine.py
@@ -97,8 +97,8 @@ def detect_mps(root, names_ids, pattern_db, mp_db=None, minister_db=None, minist
     # For bicameral era, prioritize MPs from the same chamber as the protocol
     if "chamber" in metadata:
         chamber = {'Första kammaren': 1, 'Andra kammaren':2}.get(metadata['chamber'], 0)
-        mp_db = mp_db[mp_db['chamber'] == chamber]
         mp_db_secondary = mp_db[mp_db['chamber'] != chamber]
+        mp_db = mp_db[mp_db['chamber'] == chamber]
         speaker_db = speaker_db[speaker_db['chamber'] == chamber]
 
     for tag, elem in elem_iter(root):
