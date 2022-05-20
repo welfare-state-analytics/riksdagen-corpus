989e50453af958be67ddcfd2b38ac7148b035a62
Robin <robin.saberinasseri@gmail.com>
2022-05-20 16:31:46 +0200

    feat: add filepath to intros.csv

--- pyriksdagen/dataset.py
+++ pyriksdagen/dataset.py
@@ -2,8 +2,7 @@ from transformers import BertTokenizerFast
 import torch
 
 class IntroDataset(torch.utils.data.Dataset):
-    """Dataset with cropped images and text from OCR performed on newspapers."""
-
+    ''' Dataset for predicting introductions '''
     def __init__(self, df):
         self.df = df
         self.tokenizer = BertTokenizerFast.from_pretrained('KB/bert-base-swedish-cased')
@@ -14,12 +13,8 @@ class IntroDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
 
         df_row = self.df.iloc[index]
-        xml_ids = df_row["id"]
-
-        # Text
-        ocr_text = df_row["text"]
         token_info = self.tokenizer.encode_plus(
-                                            ocr_text,                       # Sentence to encode.
+                                            df_row['text'],                       # Sentence to encode.
                                             add_special_tokens = True,      # Add '[CLS]' and '[SEP]'
                                             max_length = 50,                # Pad & truncate all sentences.
                                             padding='max_length',
@@ -28,4 +23,4 @@ class IntroDataset(torch.utils.data.Dataset):
                                             return_tensors = 'pt',          # Return pytorch tensors.
                                             )
 
-        return token_info, xml_ids
+        return token_info, df_row['id'], df_row['file_path']
