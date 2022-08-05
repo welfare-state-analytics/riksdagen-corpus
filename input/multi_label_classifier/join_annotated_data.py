797647b80afec7c471dc221a131c82a0284d719f
Robin <robin.saberinasseri@gmail.com>
2022-08-05 15:39:38 +0200

    fix: bug for joining annotated data

--- input/multi_label_classifier/join_annotated_data.py
+++ input/multi_label_classifier/join_annotated_data.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pandas as pd
 from collections import Counter
 from pathlib import Path
+import argparse
 
 
 def read_messy_csv(filepath):
@@ -18,30 +20,35 @@ def read_messy_csv(filepath):
     df = pd.DataFrame(
         data, columns=["id", "type", "tag", "x", "y", "w", "h", "content"]
     )
-    df = df.applymap(lambda x: x.strip('"'))  # remove csv artifact quotes
+    df = df.applymap(lambda x: x.strip('"')) # remove csv artifact quotes
     return df
 
 
-p = Path()
-tags = ["seg", "intro", 'note']
+def main(args):
+    p = Path()
+    tags = args.tags
+    for tag in tags:
+        right = pd.concat(
+            [read_messy_csv(f) for f in p.glob(f'input/multi_label_classifier/{tag}/*.csv')])\
+            .reset_index(drop=True
+        )
+        right[tag] = 1
+        right = right.drop('tag', axis=1)
 
-for tag in tags:
-    
-    right = pd.concat(
-        [read_messy_csv(f) for f in p.glob(f'input/multi_label_classifier/{tag}/*.csv')])\
-        .reset_index(drop=True
-    )
+        if tag == tags[0]:
+            left = right 
+        else:
+            left = left.merge(right[["id", tag]], on="id", how="left")
+            ids = [i for i, row in right.iterrows() if row['id'] not in set(left['id'])]
+            left = pd.concat([left, right.loc[ids]])
+            
+    left[tags] = left[tags].fillna(0).astype(int)
+    left = left.sort_values('id')
+    left.to_csv('input/multi_label_classifier/training_data.csv', index=False)
 
-    right[tag] = 1
 
-    if tag == tags[0]:
-        left = right
-    else:  # left join multilabel observations, concatenate others
-        left = left.merge(right[["id", tag]], on="id", how="left")
-        ids = left.loc[left[tag].notna(), "id"].tolist()
-        left = pd.concat([left, right[right["id"].isin(ids)]])
-
-df = left[left["id"].duplicated()].sort_values(by="id")
-df[tags] = df[tags].fillna(0).astype(int)
-df.to_csv('input/multi_label_classifier/training_data.csv', index=False)
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-t', '--tags', nargs='+', default=["seg", "intro", 'note'])
+    args = parser.parse_args()
+    main(args)
