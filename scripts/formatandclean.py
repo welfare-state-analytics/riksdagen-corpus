7a33c7a437b3561b43e3c74adaebdb60843a1576
ninpnin <vainoyrjanainen@icloud.com>
2022-08-30 15:59:23 +0200

    feat: rewrite protocol formatting and cleaning script

--- scripts/formatandclean.py
+++ scripts/formatandclean.py
@@ -13,65 +13,34 @@ from pyriksdagen.utils import infer_metadata
 from lxml import etree
 import pandas as pd
 import os, progressbar, argparse
+from pathlib import Path
 
-
-# TODO: remove empty tags in this script
 def main(args):
-    start_year = args.start
-    end_year = args.end
-    root = ""  # "../"
-    pc_folder = root + "corpus/"
-    folders = os.listdir(pc_folder)
-
-    mp_db = pd.read_csv(root + "corpus/members_of_parliament.csv")
-
     parser = etree.XMLParser(remove_blank_text=True)
-    for outfolder in progressbar.progressbar(folders):
-        if os.path.isdir(pc_folder + outfolder):
-            outfolder = outfolder + "/"
-            protocol_ids = os.listdir(pc_folder + outfolder)
-            protocol_ids = [
-                protocol_id.replace(".xml", "")
-                for protocol_id in protocol_ids
-                if protocol_id.split(".")[-1] == "xml"
-            ]
+    protocol_folders = list(Path(args.corpus_path).glob("protocols/*"))
+    for folder in progressbar.progressbar(protocol_folders):
+        year = folder.stem[:4]
+        year = int(year)
+        if args.start <= year <= args.end:
+            for file in folder.glob("*.xml"):
+                protocol_id = file.stem
+                with file.open() as f:
+                    root = etree.parse(f, parser).getroot()
 
-            first_protocol_id = protocol_ids[0]
-            metadata = infer_metadata(first_protocol_id)
-            year = metadata["year"]
-            if year >= start_year and year <= end_year:
-                for protocol_id in progressbar.progressbar(protocol_ids):
-                    metadata = infer_metadata(protocol_id)
-                    filename = pc_folder + outfolder + protocol_id + ".xml"
-                    root = etree.parse(filename, parser).getroot()
+                root = format_texts(root)
+                root = update_hashes(root, protocol_id)
+                root = update_ids(root, protocol_id)
+                b = etree.tostring(
+                    root, pretty_print=True, encoding="utf-8", xml_declaration=True
+                )
 
-                    years = [
-                        int(elem.attrib.get("when").split("-")[0])
-                        for elem in root.findall(
-                            ".//{http://www.tei-c.org/ns/1.0}docDate"
-                        )
-                    ]
-
-                    if not year in years:
-                        year = years[0]
-
-                    if str(year) not in protocol_id:
-                        print(protocol_id, year)
-
-                    root = format_texts(root)
-                    root = update_hashes(root, protocol_id)
-                    b = etree.tostring(
-                        root, pretty_print=True, encoding="utf-8", xml_declaration=True
-                    )
-
-                    f = open(filename, "wb")
+                with file.open("wb") as f:
                     f.write(b)
-                    f.close()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--corpus_path", type=str, default="corpus")
     parser.add_argument("--start", type=int, default=1920)
-    parser.add_argument("--end", type=int, default=2022)
+    parser.add_argument("--end", type=int, default=2021)
     args = parser.parse_args()
     main(args)
