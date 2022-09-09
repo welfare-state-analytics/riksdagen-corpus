3af8ad292c02d580c977b8e3244f828b8ac4f176
ninpnin <vainoyrjanainen@icloud.com>
2022-09-09 17:42:48 +0200

    feat: BERT classification of paragraph type

--- scripts/reclassify.py
+++ scripts/reclassify.py
@@ -35,14 +35,32 @@ def classify_paragraph(s, model, ft, dim, prior=np.log([0.8, 0.2])):
         return "u"
 
 def get_neural_classifier(model, ft, dim):
-    return (lambda paragraph: classify_paragraph(paragraph, model, ft, dim))
+    return (lambda paragraph: classify_paragraph(paragraph.text, model, ft, dim))
+
+def preclassified(df, elem):
+    xml_ns = "{http://www.w3.org/XML/1998/namespace}"
+    tei_ns = ".//{http://www.tei-c.org/ns/1.0}"
+    xml_id = f"{xml_ns}id"
+    if f"{xml_ns}id" not in elem.attrib:
+        return elem.tag.split(tei_ns)[-1]
+    
+    xml_id = elem.attrib[xml_id]
+    first_row = df[df.id == xml_id].iloc[0]
+    if first_row["preds"] == 0:
+        return "note"
+    else:
+        return "u"
+
+def get_filename_classifier(filename):
+    df = pd.read_csv(filename)
+    return (lambda paragraph: preclassified(df, paragraph))
 
 def main(args):
     parser = etree.XMLParser(remove_blank_text=True)
 
     if False:
         classifier = random_classifier
-    else:
+    elif args.method == "w2v":
         # Do imports here because they take a loong time
         from tensorflow import keras
         import fasttext, fasttext.util
@@ -51,8 +69,11 @@ def main(args):
         ft = fasttext.load_model("cc.sv." + str(dim) + ".bin")
         model = keras.models.load_model('input/segment-classifier/')
         classifier = get_neural_classifier(model, ft, dim)
+    elif args.method == "bert":
+        classifier = get_filename_classifier(args.classfile)
 
-    for protocol_path in progressbar.progressbar(list(protocol_iterators("corpus/", start=args.start, end=args.end))):
+    for protocol_path in progressbar.progressbar(list(protocol_iterators("corpus/protocols/", start=args.start, end=args.end))):
+        print(protocol_path)
         metadata = infer_metadata(protocol_path)
         root = etree.parse(protocol_path, parser).getroot()
 
@@ -68,5 +89,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--start", type=int, default=1920)
     parser.add_argument("--end", type=int, default=2022)
+    parser.add_argument("--method", type=str, default="w2v")
+    parser.add_argument("--classfile", type=str, default=None)
     args = parser.parse_args()
     main(args)
