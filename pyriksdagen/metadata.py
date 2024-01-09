602b8a9354f85b857adff18221284b95cf7fb1d8
Bob Borges <robert.d.borges@gmail.com>
2024-01-09 13:22:31 +0100

    feat: fn to add twitter to Corpus, fn to easily fetch populated Corpus class object

--- pyriksdagen/metadata.py
+++ pyriksdagen/metadata.py
@@ -224,6 +224,57 @@ class Corpus(pd.DataFrame):
 		df = self._load_metadata('party_abbreviation')
 		return abbreviate_party(self, df)
 
+	def add_twitter(self):
+		df = self._load_metadata('twitter')
+		return self.merge(df, on='swerik_id', how='left')
+
 	def clean_names(self):
 		return clean_name(self)
 
+
+
+def load_Corpus_metadata():
+	"""
+	Populates Corpus object
+	"""
+	corpus = Corpus()
+
+	corpus = corpus.add_mps()
+	corpus = corpus.add_ministers()
+	corpus = corpus.add_speakers()
+
+	corpus = corpus.add_persons()
+	corpus = corpus.add_location_specifiers()
+	corpus = corpus.add_names()
+
+	corpus = corpus.impute_dates()
+	corpus = corpus.impute_parties()
+	corpus = corpus.abbreviate_parties()
+	corpus = corpus.add_twitter()
+	corpus = corpus.clean_names()
+
+	# Clean up speaker role formatting
+	corpus["role"] = corpus["role"].replace({
+		'Sveriges riksdags talman':'speaker',
+		'andra kammarens andre vice talman':'ak_2_vice_speaker',
+		'andra kammarens förste vice talman':'ak_1_vice_speaker',
+		'andra kammarens talman':'ak_speaker',
+		'andra kammarens vice talman':'ak_1_vice_speaker',
+		'andre vice talman i första kammaren':'fk_2_vice_speaker',
+		'första kammarens talman':'fk_speaker',
+		'första kammarens vice talman':'fk_1_vice_speaker',
+		'förste vice talman i första kammaren':'fk_1_vice_speaker'
+		})
+
+	# Temporary ids
+	corpus['person_id'] = corpus['swerik_id']
+
+	# Drop individuals with missing names
+	corpus = corpus[corpus['name'].notna()]
+
+	# Remove redundancy and split file
+	corpus = corpus.drop_duplicates()
+	corpus = corpus.dropna(subset=['name', 'start', 'end'])
+	corpus = corpus.sort_values(['swerik_id', 'start', 'end', 'name'])
+
+	return corpus
