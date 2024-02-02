a5b60b26ce11d3f47d5056cddc3171c636759f72
ninpnin <vainoyrjanainen@icloud.com>
2024-02-02 14:19:33 +0100

    refactor: rewrite pipeline from scratch

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,58 +1,42 @@
 """
-Download alto XMLs from KB, convert them to parlaclarin. Saves raw data
-on disk if you want to rerun the script. 
+Convert the digital original files (1990->) into parla-clarin
 """
-import pandas as pd
-import progressbar
+import os
 from lxml import etree
+import progressbar
+import pandas as pd
 import argparse
-from pyparlaclarin.refine import (
-    format_texts,
-)
 
-from pyriksdagen.download import LazyArchive
-from pyriksdagen.export import parlaclarin_workflow_individual
-from pyriksdagen.db import load_patterns
-from pyriksdagen.refine import (
-    detect_mps,
-    find_introductions,
-    update_ids,
-    detect_date,
-)
-from pyriksdagen.utils import (
-    protocol_iterators,
-    infer_metadata,
-)
+from pyriksdagen.download import dl_kb_blocks, LazyArchive
+from pyriksdagen.export import dict_to_parlaclarin
+from pyriksdagen.utils import infer_metadata
 
+import json
+import pandas as pd
+from pathlib import Path
+import progressbar
 
 def main(args):
-    file_dbs = []
-    if args.scanned:
-        file_dbs.append(pd.read_csv("input/protocols/scanned.csv"))
-    if args.digital_originals:
-        file_dbs.append(pd.read_csv("input/protocols/digital_originals.csv"))
-    file_db = pd.concat(file_dbs)
-    file_db = file_db[file_db["year"] >= args.start]
-    file_db = file_db[file_db["year"] <= args.end]
-
-    print(file_db)
-
+    package_ids = ["prot-198990--7"]
     archive = LazyArchive()
+    for package_id in progressbar.progressbar(list(package_ids)):
+        data = infer_metadata(package_id)
+        print("metadata", data)
+        data["session"] = data["sitting"]
+        data["protocol_id"] = data["protocol"]
+        paragraphs = dl_kb_blocks(package_id, archive)
+        print()
+        print(paragraphs[0])
+        data["edition"] = args.edition
+        data["paragraphs"] = paragraphs
 
-    # Create parla-clarin files
-    parlaclarin_workflow_individual(
-        file_db, archive
-    )
+        # Create parlaclarin and write to disk
+        dict_to_parlaclarin(data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-i", "--scanned", action="store_true", help="Set this flag for protocols that were scanned.")
-    parser.add_argument("-d", "--digital_originals", action="store_true", help="Set this flag for born-digital protocols.")
-    parser.add_argument("-s", "--start", type=int, default=1920, help="Start year")
-    parser.add_argument("-e", "--end", type=int, default=2022, help="End year")
+    parser.add_argument("--start", type=int, default=1990)
+    parser.add_argument("--end", type=int, default=2022)
+    parser.add_argument("--edition", type=str, default="0.4.2")
     args = parser.parse_args()
-    if args.scanned == args.digital_originals:
-        print("\nSet either -i or -d, but not both.\n")
-        parser.print_help()
-    else:
-        main(args)
+    main(args)
